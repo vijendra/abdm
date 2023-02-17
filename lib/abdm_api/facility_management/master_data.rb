b22015a3936d9627006a4d1c230e74ef156b52a0
nithin <nithinstany@gmail.com>
2023-02-17 17:34:36 +0530

    State and district

--- lib/abdm_api/facility_management/master_data.rb
+++ lib/abdm_api/facility_management/master_data.rb
@@ -1,8 +1,12 @@
 module AbdmAPI::V1::FacilityManagement
   class MasterData < AbdmAPI::Base
 
-    def get_data(params_type)
-      response = Faraday.get("#{AbdmAPI.configuration.facility_base_url}/FacilityManagement/v1.5/facility/get-master-data?type=#{params_type}")
+    def get_short_url
+      "#{AbdmAPI.configuration.facility_base_url}/FacilityManagement/v1.5/facility"
+    end
+
+    def get_data(api_type)
+      response = Faraday.get("#{get_short_url}/get-master-data?type=#{api_type}")
  
       handle_response(response)
     end
@@ -10,6 +14,16 @@ module AbdmAPI::V1::FacilityManagement
     def get_owner_subtype(ownership_code, owner_subtype_code)
       payload = { ownershipCode: ownership_code, ownerSubtypeCode: owner_subtype_code }
       post_request(health_facility_connection, '/FacilityManagement/v1.5/facility/get-owner-subtype', payload)
+    end
+
+    def states
+      response = Faraday.get("#{get_short_url}/lgd/states")
+      handle_response(response)
+    end
+
+    def districts(state_code)
+      response = Faraday.get("#{get_short_url}/lgd/districts?stateCode=#{state_code}")
+      handle_response(response)
     end
 
   end
